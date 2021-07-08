1874a46612e803a35738a4ec759776d525dfbbe8
Jan Gevaert <jan.gevaert@gmail.com>
2021-07-08 15:52:08 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/DeclaringAndInitializingOfVariables.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive;
+
+public class DeclaringAndInitializingOfVariables {
+
+  public static void main(String[] args) {
+    //declaring and initializing in two lines.
+    int myNumber;//set to 0 by default. Declaration.
+    myNumber = 5;//Initialization.
+    System.out.println("myNumber = " + myNumber);
+  }
+
+}
